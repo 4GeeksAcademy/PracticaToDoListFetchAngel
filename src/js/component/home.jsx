79c787109c7131f8291ab6c00d8b741b737e607f
angeljimenez91 <139405887+angeljimenez91@users.noreply.github.com>
2024-01-17 19:32:33 +0000

    segundo commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 
@@ -7,6 +7,7 @@ import React, { useState } from "react";
 const Home = () => {
 	const [newTask, setNewTask] = useState("")
 	const [task, setTask] = useState([])
+
 
 	function handleChanges(event) {
 		setNewTask(event.target.value)
@@ -22,9 +23,37 @@ const Home = () => {
 	const addTasks = (event) => {
 		event.preventDefault();
 
-		if (newTask.trim() !== '') {
+		if (newTask.trim() !== '' && event.key === 'Enter') {
 
-		let body = task.concat([{"label": task, "done": false}])
+			let body = task.concat([{ "label": newTask, "done": false }])
+
+			fetch('https://playground.4geeks.com/apis/fake/todos/user/angeljimenez', {
+				method: "PUT",
+				body: JSON.stringify(body),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+
+				.then(resp => {
+					return resp.json();
+				})
+
+				.then(data => {
+					console.log(data);
+					setTask(body)
+					setNewTask('')
+				})
+				.catch(error => {
+					console.log(error)
+				})
+		}
+	}
+
+
+	function deleteTask(index) {
+
+		let body = task.filter((_, i) => i !== index);
 
 		fetch('https://playground.4geeks.com/apis/fake/todos/user/angeljimenez', {
 			method: "PUT",
@@ -40,57 +69,36 @@ const Home = () => {
 
 			.then(data => {
 				console.log(data);
-					setTask([...task, newTask])
-					setNewTask('')
+				setTask(task.filter((_, i) => i !== index));
 			})
+
 			.catch(error => {
-				console.log(error)
-			})
-		}
-	}
+				console.log(error);
+			});
 
+	};
 
-   function deleteTask(index) {
+	useEffect(() => {
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/angeljimenez')
+			.then(response => response.json())
+			.then(data => setTask(data))
+			.catch(error => console.error('Error al obtener tareas:', error));
+	}, []);
 
-	let body = task.filter((_, i) => i !== index);
-
-	fetch('https://playground.4geeks.com/apis/fake/todos/user/angeljimenez', {
-		method: "PUT",
-		body: JSON.stringify(body),
-		headers: {
-			"Content-Type": "application/json"
-		}
-	})
-
-		.then(resp => {
-			return resp.json();
-		})
-
-		.then(data => {
-			console.log(data);
-			setTask(task.filter((_, i) => i !== index));
-		})
-
-		.catch(error => {
-			console.log(error);
-		});
-
-};
-
-return (
-	<div className="container text-center">
-		<h1><i class="far fa-calendar-alt container d-inline"></i>todoList</h1>
-		<form onSubmit={addTasks}>
-			<input type="text" onChange={handleChanges} value={newTask} placeholder="¿Qué necesito hacer?" />
-		</form>
-		<ul>
-			{task.map((task, i) => (
-				<li key={i}>{task}<i class="far fa-trash-alt" onClick={() => deleteTask(i)}></i></li>
-			))}
-		</ul>
-		<div>{task.length}</div>
-	</div>
-);
+	return (
+		<div className="container text-center">
+			<h1><i className="far fa-calendar-alt container d-inline"></i>todoList</h1>
+			<form onSubmit={addTasks}>
+				<input type="text" onChange={handleChanges} onKeyUp={addTasks} value={newTask} placeholder="¿Qué necesito hacer?" />
+			</form>
+			<ul>
+				{task.map((task, i) => (
+					<li key={i}>{task.label}<i className="far fa-trash-alt" onClick={() => deleteTask(i)}></i></li>
+				))}
+			</ul>
+			<div>{task.length}</div>
+		</div>
+	);
 };
 
 export default Home;
