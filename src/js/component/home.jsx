c90e8e2c827d698f19b4d306591409825e505c79
angeljimenez91 <139405887+angeljimenez91@users.noreply.github.com>
2024-01-17 17:36:16 +0000

    primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,8 +8,6 @@ const Home = () => {
 	const [newTask, setNewTask] = useState("")
 	const [task, setTask] = useState([])
 
-
-
 	function handleChanges(event) {
 		setNewTask(event.target.value)
 	}
@@ -23,30 +21,76 @@ const Home = () => {
 
 	const addTasks = (event) => {
 		event.preventDefault();
+
 		if (newTask.trim() !== '') {
-			setTask([...task, newTask])
-			setNewTask('')
+
+		let body = task.concat([{"label": task, "done": false}])
+
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/angeljimenez', {
+			method: "PUT",
+			body: JSON.stringify(body),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+
+			.then(resp => {
+				return resp.json();
+			})
+
+			.then(data => {
+				console.log(data);
+					setTask([...task, newTask])
+					setNewTask('')
+			})
+			.catch(error => {
+				console.log(error)
+			})
 		}
 	}
 
-	const deleteTask = (index) => {
-		setTask(task.filter((_, i) => i !== index));
-	  };
 
-	return (
-		<div className="container text-center">
-			<h1><i class="far fa-calendar-alt container d-inline"></i>todoList</h1>
-			<form onSubmit={addTasks}>
-				<input type="text" onChange={handleChanges} value={newTask} placeholder="¿Qué necesito hacer?" />
-			</form>
-			<ul>
+   function deleteTask(index) {
+
+	let body = task.filter((_, i) => i !== index);
+
+	fetch('https://playground.4geeks.com/apis/fake/todos/user/angeljimenez', {
+		method: "PUT",
+		body: JSON.stringify(body),
+		headers: {
+			"Content-Type": "application/json"
+		}
+	})
+
+		.then(resp => {
+			return resp.json();
+		})
+
+		.then(data => {
+			console.log(data);
+			setTask(task.filter((_, i) => i !== index));
+		})
+
+		.catch(error => {
+			console.log(error);
+		});
+
+};
+
+return (
+	<div className="container text-center">
+		<h1><i class="far fa-calendar-alt container d-inline"></i>todoList</h1>
+		<form onSubmit={addTasks}>
+			<input type="text" onChange={handleChanges} value={newTask} placeholder="¿Qué necesito hacer?" />
+		</form>
+		<ul>
 			{task.map((task, i) => (
-					<li key={i}>{task}<i class="far fa-trash-alt" onClick={() => deleteTask(i)}></i></li>
-				))}
-			</ul>
-			<div>{task.length}</div>
-		</div>
-	);
+				<li key={i}>{task}<i class="far fa-trash-alt" onClick={() => deleteTask(i)}></i></li>
+			))}
+		</ul>
+		<div>{task.length}</div>
+	</div>
+);
 };
 
 export default Home;
